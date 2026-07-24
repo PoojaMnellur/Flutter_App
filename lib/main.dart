73d7663674441efc51fc85dcc4b7a84499aa3dcc
PoojaMnellur <poojanellur2@gmail.com>
2026-07-24 10:36:28 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 236, 233, 233),
         appBar: AppBar(
-          title: const Text('Restaurant App'),
+          title: const Text('Restaurant App2'),
           centerTitle: true,
           backgroundColor: Colors.deepOrange,
           foregroundColor: Colors.white,
